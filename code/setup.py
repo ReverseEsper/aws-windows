038209f24e237d58bdaa8ea19eaa5b342b5d7286
Adam Kurowski <adam.kurowski@stepstone.com>
2018-12-01 06:58:18 +0100

    update to ver 3

--- code/setup.py
+++ code/setup.py
@@ -3,15 +3,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='aws-ad',  
-     version='0.1',
+     version='0.3',
      scripts=['aws-ad'] ,
      author="Adam Kurowski",
      author_email="adam.kurowski.git@darevee.pl",
      description="A tool for logging into aws with ad credentials",
      long_description=long_description,
-   long_description_content_type="text/markdown",
+     use_2to3=True,
+    long_description_content_type="text/markdown",
      url="https://github.com/ReverseEsper/aws-windows",
-     install_requires=['bs4','boto3'],
+     install_requires=['bs4','boto3','configparser','argparse','requests'],
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
